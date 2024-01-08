c6fb82f3e22cabe25112a6576d8dee9d7ba8c623
Adam <139315516+ACMayer@users.noreply.github.com>
2024-01-08 20:42:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ with st.sidebar:
              
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model=tf.keras.models.load_model('####NEED TO EDIT #####mango_model.h5')
+    model=tf.keras.models.load_model('####NEED TO EDIT #####Project4.h5')
     return model
 with st.spinner('Model is being loaded..'):
     model=load_model()
